5a82d6c2ddf1b4d5233b9a233bf1ca2371ac7686
appperuplayonline-alt <appperuplayonline@gmail.com>
2025-10-16 18:14:18 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,10 @@
-const CACHE_NAME = 'radio-naranjos-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  'https://i.postimg.cc/nzVkBbwD/IMG-20251004-WA0085-1.jpg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('radio-kalidad-cache').then(cache => {
+      return cache.addAll(['./index_Radio_Kalidad_original.html', './manifest.webmanifest']);
+    })
   );
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }))
-    )
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
 });
